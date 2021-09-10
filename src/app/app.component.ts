ec68f5907ef01455ca106c8d2362f5e8c3a76326
Johnfmack <jmackay101@gmail.com>
2021-09-10 16:26:51 +1000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-
+import {Component} from '@angular/core';
+//import {Todo} from './todo';
+//import {TodoDataService} from './todo-data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
-  title = 'todo-app';
+ 
 }
